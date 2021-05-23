fafc9db184e453ba813c9e8766457413266cd3bb
Yonanni <adhanamengisteab@gmail.com>
2021-05-24 01:18:23 +0200

    Ex-04

--- M1Ex.js
+++ M1Ex.js
@@ -75,7 +75,13 @@ const splitMe = function (str){
 /* Ex.4
     Write a function called "deleteOne" which receives a string and a boolean as parameters. If the boolean value is true it should return the string without the first letter, otherwise it should remove the last one from it.
 */
-
+const deleteOne = function (str, yes){
+  if (yes){
+    return str.slice(1, str.length)
+  } else {
+    return str.slice(0, str.length - 2)
+  }
+}
 /* Ex.5
    Write a function called "onlyLetters" which receives a string as a parameter and returns it removing all the digits.
    Ex.: onlyLetters("I have 4 dogs")  => returns "I have  dogs"
