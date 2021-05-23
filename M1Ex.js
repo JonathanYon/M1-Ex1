4a258818ed28c952fd6f26cc25a13590eab661ff
Yonanni <adhanamengisteab@gmail.com>
2021-05-23 12:36:50 +0200

    Ex5

--- M1Ex.js
+++ M1Ex.js
@@ -37,7 +37,7 @@ let me = {
 /* Ex.E 
     Programmatically remove the age property from the previously create object.
 */
-
+delete me.age
 /* Ex.F 
    Programmatically add to the object me an array called "skills", containing the programming languages you know right now.
 */
