d7d3c98709ac0fde4ea854ba0769139a3422030a
Yonanni <adhanamengisteab@gmail.com>
2021-05-23 12:31:09 +0200

    Ex2 d

--- M1Ex.js
+++ M1Ex.js
@@ -19,6 +19,8 @@ let test = "what string ?";
 /* Ex.B
     Create a variable called "sum" and assign to it the result of the sum between 10 and 20.
 */
+let sum1 = 10 + 20;
+let sum = sum1;
 
 /* Ex.C 
     Create a variable called "random" and assign to it a random number between 0 and 20 (it should be randomly created at each execution).
